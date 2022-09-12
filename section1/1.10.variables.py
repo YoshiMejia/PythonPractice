5d7869eef9af32bdf980249e7bf02755ca397fe6
Yoshi <yoshimejia00@gmail.com>
2022-09-12 12:02:59 -0400

    added in post-lesson tip

--- section1/1.10.variables.py
+++ section1/1.10.variables.py
@@ -6,3 +6,18 @@ price = 2
 
 total_price = items * price
 print(total_price)
+
+# Tip
+# Tip 1 - Printing Multiple Variables 
+
+# It is also possible to print out multiple variables in one single print function by using commas between the variables. 
+# The last line of the code below contains an example:
+
+items = 10
+price = 2
+ 
+total_price = items * price
+ 
+# Below we print out three variables
+print(total_price, items, price) 
+# Output: 20 10 2
