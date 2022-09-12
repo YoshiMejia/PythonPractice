3a2259058d909ce940ae0ddbcfa9bd103cb85fe6
Yoshi <yoshimejia00@gmail.com>
2022-09-12 12:25:10 -0400

    added post-lesson notes

--- section1/1.15.list_types.py
+++ section1/1.15.list_types.py
@@ -1,2 +1,5 @@
 # list holds multiple objects. It is the same as a JS Array
 student_grades = [9.1, 8.8, 7.5]
+
+#a list can hold different types, the list below is valid as well as the one above
+student_grades = [9, "Hello", [1,2,4.33]]
