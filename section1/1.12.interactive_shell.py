6e74bd35b7d3bd71306b45ac265f59b2159e3dcd
Yoshi <yoshimejia00@gmail.com>
2022-09-12 12:08:52 -0400

    lesson 1.12 notes

---
+++ section1/1.12.interactive_shell.py
@@ -0,0 +1,7 @@
+# When using the interactive shell (terminal) the code is not saved. Use the terminal to quickly test code out. 
+# for creating a reusable program, create a file and use the text editor. 
+
+# when using the terminal, you can simply type "total_price" instead of "print(total_price)". 
+# It will print automatically without explicity calling the print function
+#  "print(total_price)" would be for when you're writing in your file.
+# to exit the terminal, type exit()
