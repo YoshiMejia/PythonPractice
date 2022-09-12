f47d2f8c2492e455e13493148b4a5fce37b0c9c2
Yoshi <yoshimejia00@gmail.com>
2022-09-12 12:01:20 -0400

    variables lesson completed

---
+++ section1/1.10.variables.py
@@ -0,0 +1,8 @@
+# no numbers at the beginning of a variable
+# no spaces in a variable, only underscores
+# spaces for readability like inbetween operators
+items = 10
+price = 2
+
+total_price = items * price
+print(total_price)
