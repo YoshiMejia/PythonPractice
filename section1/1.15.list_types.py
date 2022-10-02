a6488a548c9517bf7ff6c095da961e825917d2eb
Yoshi <yoshimejia00@gmail.com>
2022-10-02 19:28:20 -0400

    added complex list notes

--- section1/1.15.list_types.py
+++ section1/1.15.list_types.py
@@ -1,5 +1,7 @@
 # list holds multiple objects. It is the same as a JS Array
 student_grades = [9.1, 8.8, 7.5]
 
-#a list can hold different types, the list below is valid as well as the one above
+#a list can hold different types, the list below is valid as well as the one above.
+# this is called a Complex List.
 student_grades = [9, "Hello", [1,2,4.33]]
+complex_list = [4.4, 3, "ok", [1,2,3]]
