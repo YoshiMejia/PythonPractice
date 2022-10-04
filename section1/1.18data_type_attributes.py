1c8b43da1a80cc1e178242acdd639279c5dc2d43
Yoshi <yoshimejia00@gmail.com>
2022-10-04 14:00:36 -0400

    added lessons 18 and 20

---
+++ section1/1.18data_type_attributes.py
@@ -0,0 +1,17 @@
+# 1.18
+
+# dir() in the python terminal returns everything you can do with a type
+# dir(list), dir(int), dir(float), dir(str)
+# all of these will return info on that data type
+#  help(str.upper) will return documentation snippet with info on that method
+
+# 1.20
+# dir(__builtins__) returns built in functions (2 underscores on either side)
+
+# Get the mean below 
+
+student_grades = [9.1, 9.9, 5.7] # init list(array) with grades
+
+mysum = sum(student_grades) # declare mysum variable which is the sum of all the grades
+length = len(student_grades) # declare length variable which is the length of elements in the list
+mean = mysum / len # declare variable which is the mean, dividing the sum by the num of elements
