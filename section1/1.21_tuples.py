3759b8bcf44757a688b383926a8054a40b015ad9
Yoshi <yoshimejia00@gmail.com>
2022-10-08 18:39:21 -0400

    tuples examples

---
+++ section1/1.21_tuples.py
@@ -0,0 +1,6 @@
+monday_temps = (1, 3, 6)
+#this is a tuple. 
+# a tuple is just like a list but has parenthesis instead of brackets.
+# another difference is that tuples are immutable, whereas lists are mutable (can be changed)
+
+#tuples are faster than lists 
