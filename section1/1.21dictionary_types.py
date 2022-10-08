5999748014a1073c708a0a8869f6143b0ec98bd8
Yoshi <yoshimejia00@gmail.com>
2022-10-08 18:29:37 -0400

    lesson 21 notes on dictionary vs list

---
+++ section1/1.21dictionary_types.py
@@ -0,0 +1,7 @@
+# list type
+student_grades = [9.1, 9.3, 8.8]
+
+#dictionary type
+student_grades = {"Mary": 9.1, "Adam": 8.8, "John": 4.7}
+#each key/value pair in a dictionary is called an "item"
+
